dde2a1b8a456d25f023c9beb0798e8843d1d1fad
github username <github email address>
2019-05-10 21:22:29 -0700

    changed font size and width

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { Card, Image, Popup} from 'semantic-ui-react'
 import ProfilePic from './ProfilePic.png'
 
 class App extends React.Component{
-  state={cardWidth: "70%"}
+  state={cardWidth: "100%"}
   componentDidMount(){
     console.log("check compoenentdidmount")
     if(window.innerWidth < 800){
       this.setState({cardWidth: "100%"})
     }else{
-      this.setState({cardWidth: "70%"})
+      this.setState({cardWidth: "75%"})
     }
   }
   render(){
@@ -20,10 +20,10 @@ class App extends React.Component{
           <Card fluid centered raised id='main_card' style={{width: this.state.cardWidth}}>
             {/*title  section*/}
             <Card.Content>
-              <Card.Header> <Popup content=<Image src={ProfilePic} avatar size='small' position="bottom center"/> trigger={<Image src={ProfilePic} avatar size='huge'/>} /><Popup trigger={<span>Yong Liang</span>} content="This is a web page resume created by Yong Liang"/></Card.Header>
+              <Card.Header><Image src={ProfilePic} avatar size='huge' position="bottom center"/><span>Yong Liang</span></Card.Header>
               {/*About section  */}
-              <Card.Description textAlign='center' id="contact_info">510-203-6233 | yongliang24@gmail.com | <Popup content="Visit Yong's Github Page" trigger={<a href="https://github.com/YongLiang24" target="_blank" rel="noopener noreferrer">Github</a>}/> | <Popup content="Visit Yong's Blog Posts" trigger={<a href="https://dev.to/yongliang24" target="_blank" rel="noopener noreferrer">Blogs</a>} /></Card.Description><br/><br/>
-                <Card.Description textAlign='left' id="slogan">Full stack web developer with a passion for software development and new technologies. With experience in Ruby on Rails, JavaScript, and React and a background in computer science, I discovered web development through college courses. I bring strong skills in customer services and team collaborations that support a company’s success.</Card.Description><br/><br/>
+              <Card.Description textAlign='center' id="contact_info">510-203-6233 | yongliang24@gmail.com | <Popup content="Visit Yong's Github Page" trigger={<a href="https://github.com/YongLiang24" target="_blank" rel="noopener noreferrer">Github</a>}/> | <Popup content="Visit Yong's Blog Posts" trigger={<a href="https://dev.to/yongliang24" target="_blank" rel="noopener noreferrer">Blogs</a>} /></Card.Description><hr/>
+                <Card.Description textAlign='left' id="slogan">Full stack web developer with a passion for software development and new technologies, with experience in Ruby on Rails, JavaScript, and React.js and a background in computer science, I discovered web development through college courses. I bring strong skills in customer services and team collaborations that support a company’s success.</Card.Description><br/>
 
                 <Card.Content extra>
                   <Card.Header id="sub_headers">Technical Projects</Card.Header>
@@ -35,7 +35,7 @@ class App extends React.Component{
                   <Card.Description textAlign='left' className="project_description">+ Implemented administrator UI to create or delete menu items, receive online orders in real time
                   </Card.Description>
                   <Card.Description textAlign='left' className="project_description">+ Implemented customers UI to view menu, add to cart and place orders online</Card.Description>
-                  <Card.Description textAlign='left' className="project_description">+ Built all user interfaces employing React with Semantic UI</Card.Description> <br/>
+                  <Card.Description textAlign='left' className="project_description">+ Built all user interfaces employing React with Semantic UI</Card.Description>
                   {/*project 2  */}
                   <Card.Description textAlign='left' className='project_text'><Popup trigger={<strong><a href="https://github.com/YongLiang24/pictionary-online" target="_blank" rel="noopener noreferrer">Pictionary Online</a></strong>} content="Visit Pictionary Github Repository" /> |<Popup content="Visit Pictionary Online Website" trigger={<a href="https://yongliang24.github.io/pictionary-online/#/" target="_blank" rel="noopener noreferrer">Demo Link</a>}/></Card.Description>
                     <Card.Description textAlign='left' className="project_detail">Pictionary Online a simple way to play pictionary game online with friends.</Card.Description>
@@ -43,7 +43,7 @@ class App extends React.Component{
                     <Card.Description textAlign='left' className="project_description">+ Developed a Rails API with endpoints for guessing, canvas coordinates
                     </Card.Description>
                     <Card.Description textAlign='left' className="project_description">+ Built live update drawing and messages communicate with the guessing site</Card.Description>
-                    <Card.Description textAlign='left' className="project_description">+ Implemented user interface using React and CSS for styling</Card.Description><br/>
+                    <Card.Description textAlign='left' className="project_description">+ Implemented user interface using React and CSS for styling</Card.Description>
 
                     {/*project 3  */}
                     <Card.Description textAlign='left' className='project_text'><Popup trigger={<strong><a href="https://github.com/YongLiang24/translatr-frontend" target="_blank" rel="noopener noreferrer">Translatr</a></strong>} content="Visit Translatr Github Repository"/> |<Popup trigger={<a href="https://yongliang24.github.io/translatr-frontend/" target="_blank" rel="noopener noreferrer">Demo Link</a>} content="Visit Translatr Website"/></Card.Description>
@@ -70,7 +70,7 @@ class App extends React.Component{
                         <Card.Description textAlign='left' className="project_detail">Video Game Tester, July 2017 - December 2018</Card.Description>
                         <Card.Description textAlign='left' className="project_description">+ Tested games to Identify glitches and bugs</Card.Description>
                         <Card.Description textAlign='left' className="project_description">+ Documented test procedures, test cases and bug reports</Card.Description>
-                        <Card.Description textAlign='left' className="project_description">+ Collaborated with 8 other team members to complete daily tasks </Card.Description><br/>
+                        <Card.Description textAlign='left' className="project_description">+ Collaborated with 8 other team members to complete daily tasks </Card.Description>
 
                         <Card.Description textAlign='left' className='work_text'>
                           <strong>Uber Technologies</strong>, Bellevue, Seattle
@@ -78,7 +78,7 @@ class App extends React.Component{
                         <Card.Description textAlign='left' className="project_detail">Driver, May 2016 - June 2017</Card.Description>
                         <Card.Description textAlign='left' className="project_description">+ Communicated with passengers, provided safe and welcoming rides</Card.Description>
                         <Card.Description textAlign='left' className="project_description">+ Managed own schedule and accounting</Card.Description>
-                        <Card.Description textAlign='left' className="project_description">+ Maintained high customer standards and ratings </Card.Description><br/>
+                        <Card.Description textAlign='left' className="project_description">+ Maintained high customer standards and ratings </Card.Description>
 
                         <Card.Description textAlign='left' className='work_text'>
                           <strong>Fu Wah Restaurant</strong>, Lioza, PR
@@ -94,7 +94,7 @@ class App extends React.Component{
                         <Card.Description textAlign='left' className='work_text'>
                           <strong>Flatiron School - Seattle, WA  </strong> - May 2019
                         </Card.Description>
-                        <Card.Description textAlign='left' className="education_description">+ Full Stack Web Development, Ruby on Rails, JavaScript, and React.js program</Card.Description><br/>
+                        <Card.Description textAlign='left' className="education_description">+ Full Stack Web Development, Ruby on Rails, JavaScript, and React.js program</Card.Description>
 
                         <Card.Description textAlign='left' className='work_text'>
                           <strong>Lake Washington Institute of Technology - Kirkland, WA</strong> - Sep 2016- June 2018
